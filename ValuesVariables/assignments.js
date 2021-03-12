2d77722591c5a83fcfb7b93d59990b40a416c49c
Rimzim Thube <rimzimt@gmail.com>
2021-03-11 19:36:44 -0800

    Variables

---
+++ ValuesVariables/assignments.js
@@ -0,0 +1,7 @@
+let country = 'India';
+let continent = 'Asia';
+let population = '1M';
+
+console.log(country);
+console.log(continent);
+console.log(population);
---
+++ ValuesVariables/script.js
@@ -0,0 +1,7 @@
+let js = 'amazing';
+if (js === 'amazing') alert('FUN!!!')
+
+
+console.log(10 + 40 + 98)
+
+console.log('kil')
