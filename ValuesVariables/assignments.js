98d2f10f10d99c3898078d284846bfa6d41c333d
Rimzim Thube <rimzimt@gmail.com>
2021-03-11 20:20:54 -0800

    basic operators

--- ValuesVariables/assignments.js
+++ ValuesVariables/assignments.js
@@ -1,6 +1,6 @@
 const country = 'India';
 const continent = 'Asia';
-let population = '1M';
+let population = 10000;
 
 console.log(country);
 console.log(continent);
@@ -13,3 +13,14 @@ console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+const halfPopulation = population / 2;
+population += 1
+
+console.log(population);
+const FinlandPopulation = 100000;
+
+console.log(FinlandPopulation > population);
+
+const description = country + ' is in ' + continent + ', and its ' + population + ' people speak ' + language;
+console.log(description);
