018f239017607a47992eee9749aacb7dcf265792
Rimzim Thube <rimzimt@gmail.com>
2021-03-11 19:52:25 -0800

    Variables

--- ValuesVariables/assignments.js
+++ ValuesVariables/assignments.js
@@ -5,3 +5,11 @@ let population = '1M';
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let isIsland = true;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
